061161b628310e006b10ee22f5b4556fe5bda3c0
gppandey123 <gppandey9919@gmail.com>
2021-08-10 22:04:54 +0530

    Updates

---
+++ static/js/6.ff7e0e1a.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpassignment=this.webpackJsonpassignment||[]).push([[6],{318:function(n,s,t){"use strict";t.r(s);t(0);var e=t(1);s.default=function(){return Object(e.jsx)("div",{children:Object(e.jsx)("h1",{children:"404"})})}}}]);
+//# sourceMappingURL=6.ff7e0e1a.chunk.js.map
